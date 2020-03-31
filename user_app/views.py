82d3e49c848a4c31c8cd8dd1f738b4c8cc0d61c2
Tiril Grimsrud <tiril_k_g@hotmail.com>
2020-03-31 14:52:54 +0200

    implementing magazines

--- user_app/views.py
+++ user_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, reverse, get_object_or_404
 from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from library_app.models import Book, BookLoan
+from library_app.models import Book, BookLoan, Magazine, MagazineLoan
 from django.contrib.auth.models import User
 from django.utils import timezone
 
@@ -62,7 +62,8 @@ def logout(request):
 def profile(request):
     user = request.user
     bookloans = BookLoan.objects.filter(user=user)
-    context = {"bookloans": bookloans, "user": user}
+    magazineloans = MagazineLoan.objects.filter(user=user)
+    context = {"bookloans": bookloans, "magazineloans": magazineloans, "user": user}
     return render(request, "user_app/profile.html", context)
 
 @login_required
@@ -72,12 +73,23 @@ def loan_item(request, type, id):
         loaned_books_list = BookLoan.objects.filter(book = book).filter(returned_timestamp__isnull = True).count()
 
         if loaned_books_list == 0:
-            books_loaned_amount = (BookLoan.objects.filter(user=request.user) & BookLoan.objects.filter(returned_timestamp__isnull=True)).count()
-            if books_loaned_amount < book_limit:
+            loaned_books_amount = (BookLoan.objects.filter(user=request.user) & BookLoan.objects.filter(returned_timestamp__isnull=True)).count()
+            if loaned_books_amount < book_limit:
                 book.is_available = False
                 book.save()
                 BookLoan.objects.create(book=book, user=request.user)
-         
+
+    if type == "magazine":
+        magazine = get_object_or_404(Magazine, id=id)
+        loaned_magazines_list = MagazineLoan.objects.filter(magazine=magazine).filter(returned_timestamp__isnull=True).count()
+
+        if loaned_magazines_list == 0:
+            loaned_magazines_amount = (MagazineLoan.objects.filter(user=request.user) & MagazineLoan.objects.filter(returned_timestamp__isnull=True)).count()
+            if loaned_magazines_amount < magazine_limit:
+                magazine.is_available = False
+                magazine.save()
+                MagazineLoan.objects.create(magazine=magazine, user=request.user)
+            
     return HttpResponseRedirect(reverse("library_app:index"))
 
 
